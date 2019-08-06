da3bbc2aaa0eee77553f541f25a0aaabbcb755db
kerry.wang <wrasdf@gmail.com>
2019-08-06 16:37:46 +1000

    update / html ui

--- routers/root.js
+++ routers/root.js
@@ -11,16 +11,24 @@ router.get('/', (request, response) => {
       <meta charset="utf-8">
       <title>Kube Demo App With Metrics</title>
     </head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+      body{padding: 20px}
+    </style>
     <body>
       <h2> Kube Demo App With Metrics ------- ! </h2>
       <ul>
-        <li> /metrics  --> will return prometheus nodejs metrics </li>
-        <li> /health --> will retrun healthy status </li>
-        <li> /api/v1/500 --> will return 500 error </li>
-        <li> /api/v1/504 --> will return 504 error </li>
-        <li> /api/v1/499 --> will return 499 error </li>
-        <li> /api/v1/largeresp --> will return largeresp </li>
-        <li> /s3/v1/list --> list all the s3 bucket </li>
+        <li> <b>GET</b>      /metrics  --> will return prometheus nodejs metrics </li>
+        <li> <b>GET</b>      /health --> will retrun healthy status </li>
+        <li> <b>GET</b>      /api/v1/500 --> will return 500 error </li>
+        <li> <b>GET</b>      /api/v1/504 --> will return 504 error </li>
+        <li> <b>GET</b>      /api/v1/499 --> will return 499 error </li>
+        <li> <b>GET</b>      /api/v1/largeresp --> will return largeresp </li>
+        <li> <b>GET</b>      /s3/v1/list --> list all the s3 bucket </li>
+        <li> <b>POST</b>     /s3/v1/<bucket> --> create s3 bucket in AWS </li>
+        <li> <b>PUT</b>      /s3/v1/<bucket>/<key> --> create key in s3 bucket </li>
+        <li> <b>DELETE</b>   /s3/v1/<bucket>/<key> --> delete key in s3 bucket </li>
+        <li> <b>DELETE</b>   /s3/v1/<bucket> --> delete s3 bucket in AWS </li>
       </ul>
     </body>
     </html>
